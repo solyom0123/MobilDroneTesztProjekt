26d8eff6d6a0afba3da57a37ca9d332c0627d7f3
bebesi.andras <bebesi.andras@promera.systems>
2026-03-29 09:10:30 +0200

    forced set parameter manual drive fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mobildrontesztprojekt"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
